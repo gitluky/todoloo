eee68b0d7bd6f7a57155cc6be3486977a649e094
Jonathan Luk <jluk87@gmail.com>
2019-05-28 18:30:07 -0400

    added resources for User class

---
+++ db/migrate/20190528212104_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password_digest
+      t.string :name
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
