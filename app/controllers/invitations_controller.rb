8c86a95d69eb6ef71d353243e19aab9643919bee
Jonathan Luk <jluk87@gmail.com>
2019-06-03 15:52:47 -0400

    Roughly created InvitationsController methods.

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,2 +1,25 @@
 class InvitationsController < ApplicationController
+
+  def index
+    @invitations = Invitation.where(group_id: params[:group_id])
+  end
+
+  def new
+    @invitation = Invitation.new
+  end
+
+  def create
+    invitation = Invitation.create(invitation_params)
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def invitation_params
+    params.require(:invitation).permit(:group_id, :sender_id, :recipient_id)
+  end
+
 end
